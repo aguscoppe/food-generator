14e148bd53805a793f606790dd4fc140c4d82940
Agustina Coppe <48816292+aguscoppe@users.noreply.github.com>
2020-08-10 01:11:12 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+      integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+      crossorigin="anonymous"
+    />
+    <title>Random food generator</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1 class="title">
+        Random <br />
+        Food <br />
+        Generator
+      </h1>
+      <a href="enter/index.html" class="btn">Enter</a>
+    </div>
+    <footer>
+      <p>
+        Agustina Coppe &copy; 2019
+        <a href="https://github.com/aguscoppe"><i class="fab fa-github"></i></a>
+      </p>
+    </footer>
+  </body>
+</html>
